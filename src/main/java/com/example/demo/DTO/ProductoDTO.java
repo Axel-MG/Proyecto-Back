e354f7158b4c07a918d5c6ed98072d63d6aad382
Chilakiler73 <oscar.saldivar.sp@gmail.com>
2025-07-11 18:49:27 -0600

    imagen y agregar
    se agrega y ponen imagen

--- src/main/java/com/example/demo/DTO/ProductoDTO.java
+++ src/main/java/com/example/demo/DTO/ProductoDTO.java
@@ -18,10 +18,7 @@ public class ProductoDTO {
 
     @Min(value = 0)
     private int stock;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
     
     public String getNombre() {
 		return nombre;
@@ -42,7 +39,7 @@ public class ProductoDTO {
 		this.stock = stock;
 	}
 	
-	public ProductoDTO() {}
+
 
     public ProductoDTO(Long id, String nombre, double precio, int stock) {
         this.id = id;
@@ -50,41 +47,9 @@ public class ProductoDTO {
         this.precio = precio;
         this.stock = stock;
     }
-=======
->>>>>>> Stashed changes
-
-=======
-
->>>>>>> Stashed changes
-=======
-
->>>>>>> Stashed changes
-=======
-
->>>>>>> Stashed changes
     private String imagen;  // <--- AÑADE ESTO
 
-    // Getters y setters
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 
-    public double getPrecio() {
-        return precio;
-    }
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
 
     public String getImagen() {
         return imagen;
