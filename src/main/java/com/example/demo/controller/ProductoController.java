f00e525272dc3d38a0fada3af66f9bd4e7ee84bb
Chilakiler73 <oscar.saldivar.sp@gmail.com>
2025-07-14 18:39:38 -0600

    eliminar y editar producto
    Se agrego el editar y modificar productos

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,4 +39,19 @@ public class ProductoController {
         ProductoDTO nuevo = productoService.guardarConValidacion(dto);
         return new ResponseEntity<>(nuevo, HttpStatus.CREATED);
     }
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> eliminarProducto(@PathVariable Long id) {
+	    productoService.eliminarProducto(id);
+	    return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<ProductoDTO> actualizarProducto(@PathVariable Long id, @RequestBody ProductoDTO dto) {
+	    ProductoDTO actualizado = productoService.actualizarProducto(id, dto);
+	    return ResponseEntity.ok(actualizado);
+	}
+	
 }
+
+
